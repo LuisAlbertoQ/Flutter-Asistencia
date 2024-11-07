1a172431d3769307802023379128bb9b539982b5
Luchito <103071836+LuisAlbertoQ@users.noreply.github.com>
2024-11-07 16:36:42 -0600

    Firebase

--- asistencia_upeu/lib/main.dart
+++ asistencia_upeu/lib/main.dart
@@ -1,8 +1,19 @@
 import 'package:asistencia_upeu/theme/AppTheme.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 import 'login/login_google.dart';
-void main() {
+
+import 'firebase_options.dart';
+bool shouldUseFirestoreEmulator = false;
+
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  if (shouldUseFirestoreEmulator) {
+    FirebaseFirestore.instance.useFirestoreEmulator('localhost', 8080);
+  }
   runApp( MyApp());
 }
 class MyApp extends StatelessWidget{
